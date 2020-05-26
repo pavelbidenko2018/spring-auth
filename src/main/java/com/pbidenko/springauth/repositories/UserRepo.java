4465f1e284e2f0add88956e505775f9bbcb2aa55
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-05-26 19:09:27 +0200

    security dependencies and views added

--- src/main/java/com/pbidenko/springauth/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pbidenko.springauth.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.pbidenko.springauth.entity.User;
-
-public interface UserRepo extends CrudRepository<User, Integer>{
-
-}
