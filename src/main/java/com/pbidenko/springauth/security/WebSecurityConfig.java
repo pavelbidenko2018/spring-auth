6dc389f047355ca2e04d95217792f91c1b1a8110
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-06-08 20:16:30 +0200

    add typography

--- src/main/java/com/pbidenko/springauth/security/WebSecurityConfig.java
+++ src/main/java/com/pbidenko/springauth/security/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/", "/registration", "/login", "/css/*")
+			.antMatchers("/", "/registration", "/login", "/css/*", "/images/*","/fonts/*")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
